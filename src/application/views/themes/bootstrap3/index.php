332af3b2e6018e3410b4e9ec97b4145b23e90ed5
Lonnie Ezell <lonnieje@gmail.com>
2014-09-11 23:27:15 -0500

    Getting proper css and js into bootstrap theme

--- src/application/views/themes/bootstrap3/index.php
+++ src/application/views/themes/bootstrap3/index.php
@@ -12,7 +12,7 @@
     <title><?= config_item('site.name') ?></title>
 
     <!-- Bootstrap core CSS -->
-    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -23,19 +23,14 @@
 
 <body>
 
-<div class="container">
+    <div class="container">
 
-    <?= $view_content ?>
+        <?= $view_content ?>
 
-</div><!-- /.container -->
+    </div><!-- /.container -->
 
 
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="../../dist/js/bootstrap.min.js"></script>
-<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 </body>
 </html>
