632960d00f189904cc9905ef2b81b943d5feefa6
Lonnie Ezell <lonnieje@gmail.com>
2014-09-17 09:42:09 -0500

    Give us some autocompletion love.

--- src/myth/Controllers/BaseController.php
+++ src/myth/Controllers/BaseController.php
@@ -2,6 +2,53 @@
 
 namespace Myth\Controllers;
 
+/**
+ * The following properties are used to provide autocomplete for IDE's.
+ *
+ * Thanks to:  https://gist.github.com/topdown/1697338
+ *
+ * @property \CI_DB_query_builder    $db
+ * @property \CI_DB_utility          $dbutil
+ * @property \CI_DB_forge            $dbforge
+ * @property \CI_Benchmark           $benchmark
+ * @property \CI_Calendar            $calendar
+ * @property \CI_Cart                $cart
+ * @property \CI_Config              $config
+ * @property \CI_Controller          $controller
+ * @property \CI_Email               $email
+ * @property \CI_Encrypt             $encrypt
+ * @property \CI_Exceptions          $exceptions
+ * @property \CI_Form_validation     $form_validation
+ * @property \CI_Ftp                 $ftp
+ * @property \CI_Hooks               $hooks
+ * @property \CI_Image_lib           $image_lib
+ * @property \CI_Input               $input
+ * @property \CI_Lang                $lang
+ * @property \CI_Loader              $load
+ * @property \CI_Log                 $log
+ * @property \CI_Model               $model
+ * @property \CI_Output              $output
+ * @property \CI_Pagination          $pagination
+ * @property \CI_Parser              $parser
+ * @property \CI_Profiler            $profiler
+ * @property \CI_Router              $router
+ * @property \CI_Session             $session
+ * @property \CI_Sha1                $sha1
+ * @property \CI_Table               $table
+ * @property \CI_Trackback           $trackback
+ * @property \CI_Typography          $typography
+ * @property \CI_Unit_test           $unit_test
+ * @property \CI_Upload              $upload
+ * @property \CI_URI                 $uri
+ * @property \CI_User_agent          $user_agent
+ * @property \CI_Xmlrpc              $xmlrpc
+ * @property \CI_Xmlrpcs             $xmlrpcs
+ * @property \CI_Zip                 $zip
+ * @property \CI_Javascript          $javascript
+ * @property \CI_Jquery              $jquery
+ * @property \CI_Utf8                $utf8
+ * @property \CI_Security            $security
+ */
 class BaseController extends \CI_Controller
 {
     /**
