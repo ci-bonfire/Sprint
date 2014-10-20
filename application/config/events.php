59b22002a03303ab1f0812a12b79942ca333043a
Lonnie Ezell <lonnieje@gmail.com>
2014-10-19 23:22:38 -0500

    Added new Events library.

---
+++ application/config/events.php
@@ -0,0 +1,18 @@
+<?php
+
+use \Myth\Events as Event;
+
+//--------------------------------------------------------------------
+// EVENTS
+//--------------------------------------------------------------------
+// This file lets you subscribe functionality to happen at different
+// Event points throughout the application.
+//
+// These Events are separate from CodeIgniter's built-in Hooks, and are
+// slightly more flexible, using features in PHP 5.4+.
+//
+// This is a (possibly incomplete) list of events included throughout
+// the system.
+//
+//
+
---
+++ tests/unit/myth/EventTest.php
@@ -0,0 +1,193 @@
+<?php
+
+namespace Myth;
+
+use \Myth\Events as Events;
+
+class RouteTest extends \CodeIgniterTestCase
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+        Events::removeAllListeners();
+    }
+
+    protected function _after()
+    {
+    }
+
+    //--------------------------------------------------------------------
+
+    public function testListeners()
+    {
+        $callback1 = function() {};
+        $callback2 = function() {};
+
+        Events::on('foo', $callback1, EVENTS_PRIORITY_HIGH);
+        Events::on('foo', $callback2, EVENTS_PRIORITY_NORMAL);
+
+        $this->assertEquals([$callback2, $callback1], Events::listeners('foo'));
+    }
+
+    //--------------------------------------------------------------------
+
+    public function testHandleEvent()
+    {
+        $result = null;
+
+        Events::on('foo', function($arg) use(&$result) {
+            $result = $arg;
+        });
+
+        $this->assertTrue(Events::trigger('foo', ['bar']) );
+
+        $this->assertEquals('bar', $result);
+    }
+
+    //--------------------------------------------------------------------
+
+    public function testCancelEvent()
+    {
+        $result = 0;
+
+        // This should cancel the flow of events, and leave
+        // $result = 1.
+        Events::on('foo', function($arg) use (&$result) {
+            $result = 1;
+            return false;
+        });
+        Events::on('foo', function($arg) use (&$result) {
+            $result = 2;
+        });
+
+        $this->assertFalse(Events::trigger('foo', ['bar']));
+        $this->assertEquals(1, $result);
+    }
+
+    //--------------------------------------------------------------------
+
+    public function testPriority()
+    {
+        $result = 0;
+
+        Events::on('foo', function($arg) use (&$result) {
+            $result = 1;
+            return false;
+        }, EVENTS_PRIORITY_NORMAL);
+        // Since this has a higher priority, it will
+        // run first.
+        Events::on('foo', function($arg) use (&$result) {
+            $result = 2;
+            return false;
+        }, EVENTS_PRIORITY_HIGH);
+
+        $this->assertFalse(Events::trigger('foo', ['bar']));
+        $this->assertEquals(2, $result);
+    }
+
+    //--------------------------------------------------------------------
+
+    public function testPriorityWithMultiple()
+    {
+        $result = [];
+
+        Events::on('foo', function() use (&$result) {
+            $result[] = 'a';
+        }, EVENTS_PRIORITY_NORMAL);
+
+        Events::on('foo', function() use (&$result) {
+            $result[] = 'b';
+        }, EVENTS_PRIORITY_LOW);
+
+        Events::on('foo', function() use (&$result) {
+            $result[] = 'c';
+        }, EVENTS_PRIORITY_HIGH);
+
+        Events::on('foo', function() use (&$result) {
+            $result[] = 'd';
+        }, 75);
+
+        Events::trigger('foo');
+        $this->assertEquals(['c', 'd', 'a', 'b'], $result);
+    }
+
+    //--------------------------------------------------------------------
+
+    public function testRemoveListener()
+    {
+        $result = false;
+
+        $callback = function() use (&$result)
+        {
+            $result = true;
+        };
+
+        Events::on('foo', $callback);
+
+        Events::trigger('foo');
+        $this->assertTrue($result);
+
+        $result = false;
+        $this->assertTrue( Events::removeListener('foo', $callback) );
+
+        Events::trigger('foo');
+        $this->assertFalse($result);
+    }
+
+    //--------------------------------------------------------------------
+
+    public function testRemoveListenerTwice()
+    {
+        $result = false;
+
+        $callback = function() use (&$result)
+        {
+            $result = true;
+        };
+
+        Events::on('foo', $callback);
+
+        Events::trigger('foo');
+        $this->assertTrue($result);
+
+        $result = false;
+        $this->assertTrue( Events::removeListener('foo', $callback) );
+        $this->assertFalse( Events::removeListener('foo', $callback) );
+
+        Events::trigger('foo');
+        $this->assertFalse($result);
+    }
+
+    //--------------------------------------------------------------------
+
+    public function testRemoveUnknownListener()
+    {
+        $result = false;
+
+        $callback = function() use (&$result)
+        {
+            $result = true;
+        };
+
+        Events::on('foo', $callback);
+
+        Events::trigger('foo');
+        $this->assertTrue($result);
+
+        $result = false;
+        $this->assertFalse( Events::removeListener('bar', $callback) );
+
+        Events::trigger('foo');
+        $this->assertTrue($result);
+    }
+
+    //--------------------------------------------------------------------
+
+
+
+
+}
