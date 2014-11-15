5db23dc2fa1cc76bf0be59e10ca56cd808a3e583
Gwenael G <gwenael.gallon@mac.com>
2014-11-15 13:42:59 +0100

    Auth Login_model use language file for label name

--- myth/CIModules/auth/helpers/password_helper.php
+++ myth/CIModules/auth/helpers/password_helper.php
@@ -18,7 +18,7 @@ if (! function_exists('isStrongPassword'))
         if (! $strong)
         {
             if (isset(get_instance()->form_validation)) {
-                get_instance()->form_validation->set_message('isStrongPassword', lang('auth.pass_not_strong'));
+                get_instance()->form_validation->set_message('isStrongPassword', 'lang:auth.pass_not_strong');
             }
             return false;
         }
