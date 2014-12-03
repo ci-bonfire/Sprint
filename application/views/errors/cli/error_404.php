520b0cdecb87a32d5d977995f549fa6f7cc89fec
Lonnie Ezell <lonnieje@gmail.com>
2014-12-02 23:32:13 -0600

    Show the CLI string in 404 errors when calling from the CLI.

--- application/views/errors/cli/error_404.php
+++ application/views/errors/cli/error_404.php
@@ -28,3 +28,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 echo \Myth\CLI::error("\n\t404 ERROR: $heading");
 echo \Myth\CLI::write("$message\n");
+echo \Myth\CLI::write( \Myth\CLI::cli_string() );
