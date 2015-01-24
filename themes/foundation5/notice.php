f45c5d5c2a136ade951cd9699e1e247545bfa87b
Gwenael G <gwenael.gallon@mac.com>
2015-01-24 02:51:43 +0100

    Fix Notice display to renderJSON method and add missing notice view (by default Bootstrap theme)

--- themes/foundation5/notice.php
+++ themes/foundation5/notice.php
@@ -1,6 +1,11 @@
 <?php if (isset($notice) && ! empty($notice)) : ?>
-    <div class="alert-box <?= $notice['type'] ?>" data-alert role="alert">
+    <div class="alert-box <?= $notice['type'] ?>" data-alert id="notice" role="alert">
         <?= $notice['message'] ?>
         <a href="#" class="close">&times;</a>
     </div>
+
+<?php else: ?>
+
+    <div id="notice"></div>
+
 <?php endif; ?>
