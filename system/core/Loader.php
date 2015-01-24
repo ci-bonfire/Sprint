16dcf26a711225b4eb89354b46d91ad4dcd24ca1
Gwenael G <gwenael.gallon@mac.com>
2015-01-24 02:15:37 +0100

    Update CodeIgniter Don't try to include config/autoload.php if it doesn't exist

--- system/core/Loader.php
+++ system/core/Loader.php
@@ -1200,7 +1200,11 @@ class CI_Loader {
 	 */
 	protected function _ci_autoloader()
 	{
-		include(APPPATH.'config/autoload.php');
+		if (file_exists(APPPATH.'config/autoload.php'))
+		{
+			include(APPPATH.'config/autoload.php');
+		}
+
 		if (file_exists(APPPATH.'config/'.ENVIRONMENT.'/autoload.php'))
 		{
 			include(APPPATH.'config/'.ENVIRONMENT.'/autoload.php');
