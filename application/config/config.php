ef61eaa7de24eebb53035d6522dbe5fb0ae93aad
Gwenael Gallon <gwenael.gallon@mac.com>
2015-02-21 10:08:57 +0100

    Commit missing in last update for Add 'sess_regenerate_destroy' setting

--- application/config/config.php
+++ application/config/config.php
@@ -368,6 +368,7 @@ $config['sess_expiration'] = 7200;
 $config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
+$config['sess_regenerate_destroy'] = FALSE;
 
 /*
 |--------------------------------------------------------------------------
