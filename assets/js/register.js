6e2f68b0367f3639d6ef8178ebf17ca2c7272722
IrcDirk <ircdirk@gmail.com>
2015-03-29 18:01:33 +0200

    Some url fixing for application in subfolder.

--- assets/js/register.js
+++ assets/js/register.js
@@ -13,7 +13,7 @@ $(document).on('keyup', '#password', function(e) {
     }
 
     $.getJSON(
-        '/password_check/'+ current_password,
+        'password_check/'+ current_password,
         function (data)
         {
             if (data.status == 'pass')
