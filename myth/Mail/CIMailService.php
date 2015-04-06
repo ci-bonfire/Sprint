70e20c708c655f8f722d1c33e933ae65b68da768
Lonnie Ezell <lonnieje@gmail.com>
2015-04-05 21:47:11 -0500

    CIMailService now properly sets the email format based on the available messages. So, if a text version only is declared, will send as text, but if an email version is found, will send as email with the text version as an alternate, if available. Fixes #82

--- myth/Mail/CIMailService.php
+++ myth/Mail/CIMailService.php
@@ -65,7 +65,13 @@ class CIMailService implements MailServiceInterface {
      */
     public function send($clear_after=true)
     {
-        return $this->ci->email->send($clear_after);
+        $this->ci->email->mailtype = $this->format;
+
+        $result = $this->ci->email->send(false);
+
+        die($this->ci->email->print_debugger() );
+
+        return $result;
     }
 
     //--------------------------------------------------------------------
@@ -204,6 +210,8 @@ class CIMailService implements MailServiceInterface {
     {
         $this->ci->email->message($message);
 
+        $this->format = 'html';
+
         return $this;
     }
 
@@ -218,6 +226,8 @@ class CIMailService implements MailServiceInterface {
     public function text_message($message)
     {
         $this->ci->email->set_alt_message($message);
+
+        $this->format = 'text';
 
         return $this;
     }
